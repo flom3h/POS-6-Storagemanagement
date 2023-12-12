9c360733dfe2028c8840bdcf76bf0800729ef696
flom3h <pieles.florian@gmail.com>
2023-12-12 21:28:33 +0100

    Add project files.

---
+++ Part.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Console
+{
+    internal class Part
+    {
+        #region fieldvar
+        private int id;
+        private int maxQuantity;
+        private string name;
+        private double pricePerPiece;
+        private int quantity;
+        #endregion
+
+        #region Eigenschaften
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Quantity
+        {
+            get { return quantity; }
+            set { quantity = value; }
+        }
+
+        public int MaxQuantity
+        {
+            get { return maxQuantity; }
+            set { maxQuantity = value; }
+        }
+
+        public double PricePerPiece
+        {
+            get { return pricePerPiece; }
+            set { pricePerPiece = value; }
+        }
+        #endregion
+
+        #region Const
+        public Part(int id, string name, double pricePerPiece)
+        {
+            this.Id = id;
+            this.Name = name;
+            PricePerPiece = pricePerPiece;
+
+        }
+        public Part(int id, string name, double pricePerPiece, int quanitity, int maxQuantity)
+        {
+            this.Id = id;
+            Name = name;
+            PricePerPiece = pricePerPiece;
+            Quantity = quanitity;
+            MaxQuantity = maxQuantity;
+        }
+        #endregion
+
+        #region methods
+        public int RemoveParts(int amount)
+        {
+            if (Quantity - amount < 0)
+            {
+                Console.WriteLine($"[{Name}] Error: Can not remove {amount}parts, only {Quantity} available.");
+                return 0;
+            }
+            else
+            {
+                Quantity -= amount;
+                Console.WriteLine($"[{Name}] Removed: {amount} parts.");
+                return amount;
+            }
+        }
+
+        public void SetMaxQuantity(int newMaxQuantity)
+        {
+
+            if (Quantity > newMaxQuantity)
+            {
+                throw new Exception($"[{Name}] Error: Storage limit reached.");
+            }
+
+            else
+            {
+                MaxQuantity = newMaxQuantity;
+                Console.WriteLine($"[{Name}] Max quantity set to {newMaxQuantity}");
+            }
+        }
+
+        public void AddParts(int amount)
+        {
+            if (Quantity + amount > MaxQuantity)
+            {
+                int amountToAdd = MaxQuantity - Quantity;
+                Quantity += amountToAdd;
+                Console.WriteLine($"[{Name}] Error: Storage limit reached. Added only {amountToAdd} of {amount} parts.");
+            }
+            else
+            {
+                Quantity += amount;
+                Console.WriteLine($"[{Name}] Added: {amount} parts");
+            }
+        }
+
+        public int GetMaxQuantity()
+        {
+            return Quantity;
+        }
+
+        public double GetPrice()
+        {
+            return quantity * pricePerPiece;
+        }
+
+        public void Output()
+        {
+            Console.WriteLine(this.ToString());
+        }
+
+        public override string ToString()
+        {
+            return $"=============== PART: {Id} =============== \r\n" +
+                $" Name: {Name}, Value: {PricePerPiece} \r\n" +
+                $" Storage: {Quantity}/{MaxQuantity} \r\n" +
+                $" ======================================= \r\n";
+        }
+        #endregion
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,102 @@
+﻿using System.Xml.Linq;
+
+namespace C__Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Part part1 = new Part(1, "Resistor", 450, 0, 100);
+            Part part2 = new Part(2, "Capacitor", 12, 100, 1000);
+
+            Console.WriteLine($"{part1}");
+            Console.WriteLine($"{part2}");
+
+            while (true)
+            {
+                int numParts;
+                string opt = PrintMenu1();
+                string selectionStr = PrintMenu2();
+
+                try
+                {
+                    if (opt == "entf")
+                    {
+                        Console.Write("Wie viele Parts?");
+                        numParts = Convert.ToInt32(Console.ReadLine());
+
+                        if (selectionStr == "1")
+                        {
+                            part1.RemoveParts(numParts);
+                            part1.Output();
+                        }
+
+                        else if (selectionStr == "2") 
+                        {
+                            part2.RemoveParts(numParts);
+                            part2.Output();
+                        }
+                    }
+                    else if (opt == "add")
+                    {
+                        Console.Write("Wie Viele Parts?");
+                        numParts = Convert.ToInt32(Console.ReadLine());
+
+                        if (selectionStr == "1")
+                        {
+                            part1.AddParts(numParts);
+                            part1.Output();
+                        }
+
+                        else if (selectionStr == "2")
+                        {
+                            part2.AddParts(numParts);
+                            part2.Output();
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    continue;
+                }
+            }
+        }
+
+        static string PrintMenu1()
+        {
+            string menu1 = @"
+Folgende Möglichkeiten hast du:
+    entf - Entferne Teile aus dem Lager
+    add  - Füge Teile dem Lager hinzu";
+
+            Console.WriteLine(menu1);
+
+            string selectedOption;
+            do
+            {
+                Console.Write("Deine Wahl: ");
+                selectedOption = Console.ReadLine();
+            } while (selectedOption != "entf" && selectedOption != "add");
+            return selectedOption;
+        }
+
+        static string PrintMenu2()
+        {
+            string menu2 = @"
+Wähle das Lager aus das du Bearbeiten möchtest:
+    1 : 
+    2 :";
+            Console.WriteLine(menu2);
+
+            string selectedStorage;
+            do
+            {
+                Console.Write("Deine Wahl: ");
+                selectedStorage = Console.ReadLine();
+            } while (selectedStorage != "1" && selectedStorage != "2");
+            return selectedStorage;
+        }
+    }
+
+}
